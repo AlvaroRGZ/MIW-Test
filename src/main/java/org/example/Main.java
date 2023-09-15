d3f2b513dbd03fdc989fd872d377754e5a77f89f
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2023-09-15 19:26:43 +0200

    Solved iterations attribute #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    int ITERATIONS = 10;
+    static int ITERATIONS = 10;
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
